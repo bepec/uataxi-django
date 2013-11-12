4e3b3e3beeb8f92824ae3b1bcba2b257bf9ebde8
Alex Vedmidsky <loafshock@gmail.com>
2013-11-12 10:24:28 +0200

    issue #1: introduce basic models

--- uataxi/taxi/models.py
+++ uataxi/taxi/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class PhoneOperator(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class TaxiService(models.Model):
+    name = models.CharField(max_length=10)
+    title = models.CharField(max_length=64)
+    city = models.ForeignKey(City)
+
+
+class PhoneNumber(models.Model):
+    number = models.CharField(max_length=16)
+    taxi = models.ForeignKey(TaxiService)
+    operator = models.ForeignKey(PhoneOperator)
+    callback = models.BooleanField(default=False)
+
+
+class CallbackOperator(models.Model):
+    number = models.ForeignKey(PhoneNumber)
+    operator = models.ForeignKey(PhoneOperator)
