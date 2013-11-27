ca13e11647c7ab9d6be74be2728faca10fe94cfe
Alex Vedmidsky <loafshock@gmail.com>
2013-11-27 17:44:06 +0200

    issue #5: simple templates; issue #9: improve views

--- taxi/views.py
+++ taxi/views.py
@@ -1,13 +1,27 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from taxi import models
 
 
 def index(request):
-    return HttpResponse("Taxi will be here.")
+    context = {
+        'city_list': models.City.objects.order_by('name')
+    }
+    return render(request, 'taxi/index.html', context)
 
 
 def city(request, city_id):
-    return HttpResponse("You're now in the city of %s" % city_id)
+    context = {
+        'city': models.City.objects.get(id=city_id),
+        'taxi_list': models.TaxiService.objects.filter(city=city_id)
+    }
+    return render(request, 'taxi/city.html', context)
 
 
 def taxi(request, taxi_id):
-    return HttpResponse("It's a taxi %s" % taxi_id)
+    taxi = models.TaxiService.objects.get(id=taxi_id)
+    context = {
+        'taxi': taxi,
+        'city': taxi.city,
+        'phone_list': models.PhoneNumber.objects.filter(taxi=taxi_id),
+    }
+    return render(request, 'taxi/details.html', context)
